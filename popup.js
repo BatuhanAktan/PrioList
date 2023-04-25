baf8d79d8cbf1ea0ca13f6c2a015ea19779744a9
BatuhanAktan <batuhanaktan@gmail.com>
2023-04-25 03:24:40 -0400

    Server Side Handling Completed

--- popup.js
+++ popup.js
@@ -2,8 +2,6 @@ const addBtn = document.querySelector('.navbttn');
 const clearBtn = document.querySelector('.navbttnclear');
 const orgBtn = document.querySelector('.navbttnro');
 
-const puppeteer = require('puppeteer');
-
 let importance = {"Health & Personal Care":1,"Amazon Device Accessories":3, "Amazon Kindle":3, "Automotive & Powersports":3,"Baby Products (excluding apparel)":1, "Beauty":3, "Books":3, "Camera & Photo":3,"Cell Phones & Accessories":3,"Collectible Coins":4,"Consumer Electronics":3,"Entertainment Collectibles":3,"Fine Art":4,"Grocery & Gourmet Food":1,"Health & Personal Care":1,"Home & Garden,Independent Design":3,"Industrial & Scientific":3,"Kindle Accessories and Amazon Fire TV Accessories":4,"Major Appliances":2,"Music":3,"Musical Instruments":3,"Office Products":2,"Outdoors":3,"Personal Computers":2,"Pet Supplies":2,"Software":2,"Sports":2,"Sports Collectibles":4,"Tools & Home Improvement":2,"Toys & Games":2,"Video":3, "DVD & Blu-ray":3,"Video Games":3,"Watches":4};
 
 let list = [];
@@ -21,9 +19,49 @@ chrome.storage.sync.get(['data'], function(result) {
 	});
 
 
-addBtn.onclick = ()=>{
-	addList(id+1 , "New Item" , 12.25, "https://images-na.ssl-images-amazon.com/images/I/71-U4ZHnLAL._AC_SY355_.jpg", "Health & Personal Care");
-	id++; 
+addBtn.onclick = async ()=>{
+	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+		let url = tabs[0].url;
+		console.log("URL", url);
+		const search = async (url) => {
+			url  = {url};
+			console.log("Trying Content");
+			const content = await fetch('http://localhost:3001/prio', {
+				method: 'POST',
+				headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(url)
+			});
+
+			let returnContent = await content.json();
+			console.log("returned Content");
+			const o = async () => {
+
+					let status = await JSON.parse(returnContent.status);
+					console.log(status);
+					if (status === 1){
+
+						let title = await JSON.stringify(returnContent.title.title);
+						title = title.slice(1,30) + "...";
+						let price = await JSON.stringify(returnContent.price.price);
+						price = price.slice(1,-1);
+						let img  = await JSON.stringify(returnContent.img.img);
+
+						console.log("In response");
+						console.log(img);
+						addList(id+1, title, price, img, "Health");
+						id++;
+					}else{
+						console.log("Cannot Add");
+					}
+			}
+
+			o();
+		}
+
+		search(url);
+	});
 }
 
 
@@ -107,7 +145,7 @@ function addList(ID, itemName, itemPrice, itemPicUrl, itemCat){
 
 
 	const itmImg = document.createElement("SPAN");
-	itmImg.innerHTML = `<img class="productImg" src="${itemPicUrl}"alt="Product Image">`;
+	itmImg.innerHTML = `<img class="productImg" src=${itemPicUrl}alt="Product Image">`;
 	newDiv.appendChild(itmImg);
 
 
@@ -149,14 +187,9 @@ function addList(ID, itemName, itemPrice, itemPicUrl, itemCat){
 
 
 function loadList(list){
-	list.forEach(function(item){
-		addList(item.id, item.name, item.price, item.imgUrl);
-	});
+	if (list){
+		list.forEach(function(item){
+			addList(item.id, item.name, item.price, item.imgUrl);
+		});
+	}
 }
-
-async function scrape(){
-	var res = await fetch ('https://www.amazon.ca/Apple-AirPods-Charging-Latest-Model/dp/B07PXGQC1Q/?_encoding=UTF8&pd_rd_w=tqC1R&pf_rd_p=708c5fba-fb35-4548-a6c6-bccf7edf8384&pf_rd_r=F5KTHHKYJE0SC161554D&pd_rd_r=4a3c5ed3-8248-4634-b4b5-54e7f4b25827&pd_rd_wg=P8FkU&ref_=pd_gw_trq_ed')\
-	var text = await res.text(/(?<span id="productTitle" class="a-size-large product-title-word-break">)(.*?)(?=<\/span>)/g);
-	return text.match()
-}
-console.log(scrape())
